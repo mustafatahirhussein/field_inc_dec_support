f8f5b86a33292996b6b8f24d4b2146c67ae25e11
Mustafa <mustafatahirhussein@gmail.com>
2021-11-26 23:02:26 +0500

    Commit at 11:01pm & 26/11/21, added example code

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+//Copyright 2021. All rights reserved
+
+import 'package:flutter/material.dart';
+import 'package:field_inc_dec_support/field_inc_dec_support.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example of Package"),
+      ),
+      body: Center(
+        child: FieldIncDecSupport(
+          elevation: 2,
+          backColor: Colors.blue,
+          value: 0,
+          maxLimit: 99,
+        ),
+      ),
+    );
+  }
+}
